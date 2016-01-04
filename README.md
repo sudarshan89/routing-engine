9de55af24098b41153fcdd4d9eedbe6d4e6a7e96
Sudarshan <sudarshan89@outlook.com>
2016-01-04 18:21:45 +1300

    Added README.md and updated tests to use interface instead of concrete implementation.

---
+++ README.md
@@ -0,0 +1,20 @@
+# routing-engine
+==
+A opinionated implementation of a [Content-Based-Router] (http://www.enterpriseintegrationpatterns.com/patterns/messaging/ContentBasedRouter.html)
+
+This implementation executes routing criteria and selects a matching endpoint to which the request should be routed.
+
+As a user of the library you create a route register like the below
+
+IRoutesRegister routesRegister = RoutesRegister.newRegister()
+                .addRoute(newRoute(huonEndpoint, new SoapActionCriteria("simpleHUONSOAPAction")))
+                .addRoute(newRoute(exigenEndpoint, new SoapActionCriteria("simpleExigenSOAPAction")));
+                
+A router register holds multiple routes where each route is composed of an endpoint and a criteria.
+                
+When a request is received by the library it uses the routes register to determine the criteria which is satisfies this request
+and returns the corresponding the endpoint.
+
+Example routes configuration can be found in the tests [here] (https://github.com/sudarshan89/routing-engine/blob/master/src/test/java/nz/co/iag/engine/SimpleRoutingEngineTest.java)
+                
+![Routing Engine in a picture](https://goo.gl/NPJch0 "Routing Engine")                 
--- src/test/java/nz/co/iag/engine/SimpleRoutingEngineTest.java
+++ src/test/java/nz/co/iag/engine/SimpleRoutingEngineTest.java
@@ -11,14 +11,14 @@ public class SimpleRoutingEngineTest {
 
     @Test(expected = TooManyMatchingRoutesException.class)
     public  void givenEngineIsInStrictModel_whenEngineDetectsMultipleMatchingRoutes_itShouldThrowTooManyMatchingRoutesException(){
-        RoutesConfiguration routesConfiguration = new RoutesConfiguration();
+        IRoutesConfiguration routesConfiguration = new RoutesConfiguration();
         final IRoutingEngine routeEngine = routesConfiguration.getRouteEngine();
         routeEngine.selectEndpoint("simpleAmigoSOAPAction","dummyPayload",true);
     }
 
     @Test
     public  void givenEngineIsInLenientModel_whenEngineDetectsMultipleMatchingRoutes_itShouldReturnFirstSelectedRoute(){
-        RoutesConfiguration routesConfiguration = new RoutesConfiguration();
+        IRoutesConfiguration routesConfiguration = new RoutesConfiguration();
         final IRoutingEngine routeEngine = routesConfiguration.getRouteEngine();
         final Endpoint selectedEndpoint = routeEngine.selectEndpoint("simpleAmigoSOAPAction", "dummyPayload", false);
         assertThat(selectedEndpoint.getUrl().toString()).contains("amigo.com");
@@ -28,14 +28,14 @@ public class SimpleRoutingEngineTest {
 
     @Test(expected = NoMatchingRoutesFoundException.class)
     public  void whenEngineDetectsNoMatchingRoutes_itShouldThrowNoMatchingRoutesException(){
-        RoutesConfiguration routesConfiguration = new RoutesConfiguration();
+        IRoutesConfiguration routesConfiguration = new RoutesConfiguration();
         final IRoutingEngine routeEngine = routesConfiguration.getRouteEngine();
         routeEngine.selectEndpoint("simplePulseSOAPAction", "dummyPayload", false);
     }
 
     @Test
     public  void whenEngineDetectsExactlyOneMatchingRoute_itShouldReturnMatchedRoute(){
-        RoutesConfiguration routesConfiguration = new RoutesConfiguration();
+        IRoutesConfiguration routesConfiguration = new RoutesConfiguration();
         final IRoutingEngine routeEngine = routesConfiguration.getRouteEngine();
         final Endpoint selectedEndpoint = routeEngine.selectEndpoint("simpleHUONSOAPAction", "dummyPayload", true);
         assertThat(selectedEndpoint.getUrl().toString()).contains("huon.com");
